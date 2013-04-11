a20482b03443a6ca7bc88e0122a86831987a9395
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-04-11 09:50:40 +0200

    Added test for various gems in db/seed so that formats are based on availability of a gem to support the format. Also added more default formats.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,55 @@ end
   TemplateLocale.find_or_create_by_code(l)
 end
 
-[
-  { code: 'markdown' },
+# These formats are always available
+formats = [
   { code: 'erb' },
-].each do |f|
+  { code: 'html' },
+  { code: 'str' },
+  { code: 'plain' },
+]
+
+# These formats are only available if a certain gem is available
+# So we try to load the gem. If success add the given code, otherwise do
+# nothing.
+# Some formats may be available through several gems.
+# And some gems may support several formats.
+checks = [
+  { gem: 'asciidoctor',   code: 'ad' },
+  { gem: 'bluecloth',     code: 'markdown' },
+  { gem: 'builder',       code: 'builder' },
+  { gem: 'coffee_script', code: 'coffee' },
+  { gem: 'creole',        code: 'creole' },
+  { gem: 'creole',        code: 'wiki' },
+  { gem: 'csv',           code: 'rcsv' },
+  { gem: 'erubis',        code: 'erubis' },
+  { gem: 'etanni',        code: 'etn' },
+  { gem: 'haml',          code: 'haml' },
+  { gem: 'kramdown',      code: 'markdown' },
+  { gem: 'less',          code: 'less' },
+  { gem: 'liquid',        code: 'liquid' },
+  { gem: 'markaby',       code: 'mab' },
+  { gem: 'maruku',        code: 'markdown' },
+  { gem: 'nokogiri',      code: 'nokogiri' },
+  { gem: 'radius',        code: 'radius' },
+  { gem: 'rdiscount',     code: 'markdown' },
+  { gem: 'rdoc',          code: 'rdoc' },
+  { gem: 'redcarpet',     code: 'redcarpet' },
+  { gem: 'redcloth',      code: 'textile' },
+  { gem: 'sass',          code: 'sass' },
+  { gem: 'wikicloth',     code: 'mediawiki' },
+  { gem: 'wikicloth',     code: 'wiki' },
+  { gem: 'yajl',          code: 'yajl' },
+  { gem: 'slim',          code: 'slim' },
+]
+checks.each do |check|
+  begin
+    require check[:gem]
+    formats << { code: check[:code] }
+  rescue LoadError => boom
+  end
+end
+
+formats.uniq.each do |f|
   TemplateFormat.find_or_create_by_code(f)
 end
