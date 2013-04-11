09f932ebce5437fd582d8bf6536fbdf4b1dd147c
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-04-11 09:08:33 +0200

    Added current_template to template model.

--- app/models/template.rb
+++ app/models/template.rb
@@ -4,7 +4,8 @@ class Template < ActiveRecord::Base
   belongs_to :template_locale
   just_define_datetime_picker :valid_from, :add_to_attr_accessible => true
   just_define_datetime_picker :valid_until, :add_to_attr_accessible => true
-  attr_accessible :body, :code
+  attr_accessible :body, :code, :template_format_id, :template_locale_id,
+    :template_type_id
 
   validates :code, :presence => true
   validates :body, :presence => true
@@ -21,4 +22,11 @@ class Template < ActiveRecord::Base
     I18n.t code, :scope => 'tsushin.code.template'
   end
 
+  def self.current_template (name, type, locale = nil)
+    now = Time.now
+    type_id = TemplateType.find_by_code(type).id
+    locale_id = TemplateLocale.find_by_code(locale || ::Tsushin::Application::config.i18n.default_locale).id
+    Template.where("code = ? AND template_type_id = ? AND template_locale_id = ? AND valid_from <= ? AND valid_until >= ?",
+       name, type_id, locale_id, now, now).first
+  end
 end
